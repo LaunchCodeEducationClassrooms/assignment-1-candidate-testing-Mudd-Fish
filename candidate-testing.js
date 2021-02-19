846185c9cec7fbe9c7380a80e928e88c5ae7dec1
Samuel Mudd <samuel.mudd@gmail.com>
2021-02-19 05:01:07 +0000

    strings

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,11 +3,11 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
-let candidateAnswer;
+let candidateAnswer = "";
 let questions = [ "Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
 let candidateAnswers = [];
@@ -64,7 +64,6 @@ function askQuestion() {
   function runProgram() {
     askForName();
     // TODO 1.1c: Ask for candidate's name //
-    console.log("Candidate Name: " + candidateName);
     askQuestion();
     gradeQuiz(this.candidateAnswers);
   }
